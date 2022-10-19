6d405d902aa221beb9d6988188a63144cdd34ad3
Preston Hernandez <112985737+Preston-Hernadez@users.noreply.github.com>
2022-10-19 14:23:27 -0700

    Update RenameThisClass.java

--- RenameThisClass.java
+++ RenameThisClass.java
@@ -9,5 +9,20 @@ public class RenameThisClass
 {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        int i;
+        float f = 2.5f;
+        boolean flag = true;
+        i = 10;
+        f = i + f;
+        flag = !flag;
+        i = i + 1;
+        System.out.println("i");
+        System.out.println("f");
+        System.out.println("flag");
+        String string1 = "I am a string";
+        String string2 = string1;
+        String string3 = "I am also a string";
+        boolean stringsEqual =  string1 == string3;
     }
 }
